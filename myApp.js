662127ad2dbaad635130806c672f396a31df636d
Stefan Ramač <80068393+stefanramac@users.noreply.github.com>
2022-09-10 17:32:09 +0200

    testnode3 heroku

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ let app = express();
 var bGround = require('fcc-express-bground');
 
 bGround.log("Hello World");
+console.log("Hello World");
 
 
 
--- server.js
+++ server.js
@@ -57,5 +57,5 @@ if (!process.env.DISABLE_XORIGIN) {
 //Listen on port set in environment variable or default to 3000
  var port = process.env.PORT || 3000;
  bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
- bGround.log('Node is listening on port '+ port + '...')
+//  bGround.log('Node is listening on port '+ port + '...')
 });
